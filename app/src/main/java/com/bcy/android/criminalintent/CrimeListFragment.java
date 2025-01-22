b25a532695cc0ebe1fc3b6c53defe3e1f63d8fad
bcy <626813534@qq.com>
2025-01-23 01:26:28 +0800

    在构造方法中实例化视图组件

--- app/src/main/java/com/bcy/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bcy/android/criminalintent/CrimeListFragment.java
@@ -1,11 +1,10 @@
 package com.bcy.android.criminalintent;
 
 import android.os.Bundle;
-//import android.support.v7.widget.LinearLayoutManager;
-//import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -18,8 +17,14 @@ public class CrimeListFragment extends Fragment {
     private CrimeAdapter mAdapter;
 
     private class CrimeHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
+
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
         }
     }
 
