33495f71516b07e2f101e43a3eea258a23a291d8
bcy <626813534@qq.com>
2025-02-03 21:27:27 +0800

    10-2 创建newIntent方法

--- app/src/main/java/com/bcy/android/criminalintent/MainActivity.java
+++ app/src/main/java/com/bcy/android/criminalintent/MainActivity.java
@@ -4,10 +4,22 @@ package com.bcy.android.criminalintent;
 //
 //import androidx.activity.EdgeToEdge;
 //import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.fragment.app.Fragment;
+
+import java.util.UUID;
 //import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends SingleFragmentActivity {
+    public static final String EXTRA_CRIME_ID = "com.bcy.android.criminalintent_crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, MainActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
