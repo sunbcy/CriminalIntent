9c2023c396ad92aa7bf53933a8d790bdf167526c
bcy <626813534@qq.com>
2025-02-04 16:41:27 +0800

    11.1 创建ViewPager

---
+++ app/src/main/java/com/bcy/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,14 @@
+package com.bcy.android.criminalintent;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
+}
