ff4199e201a138ea45e2b05c0c49154be2070504
bcy <626813534@qq.com>
2025-02-04 21:35:33 +0800

    11-2 设置pager adapter

--- app/src/main/java/com/bcy/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bcy/android/criminalintent/CrimePagerActivity.java
@@ -3,12 +3,39 @@ package com.bcy.android.criminalintent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.List;
 
 public class CrimePagerActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
 }
