df6093d26cb3af79009acbd7f28f33998e0b8d20
bcy <626813534@qq.com>
2025-02-05 02:32:39 +0800

    11-6 设置初始分页显示项

--- app/src/main/java/com/bcy/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bcy/android/criminalintent/CrimePagerActivity.java
@@ -33,7 +33,7 @@ public class CrimePagerActivity extends AppCompatActivity {
 
         UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
 
-        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+        mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
 
         mCrimes = CrimeLab.get(this).getCrimes();
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -50,5 +50,12 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return mCrimes.size();
             }
         });
+
+        for (int i = 0; i < mCrimes.size(); i++) {
+            if (mCrimes.get(i).getId().equals(crimeId)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
     }
 }
