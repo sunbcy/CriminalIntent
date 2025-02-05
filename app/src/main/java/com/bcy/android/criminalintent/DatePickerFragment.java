d3e0b67c01e244477904cac8221ce76d123c025d
bcy <626813534@qq.com>
2025-02-06 00:19:05 +0800

    12-2 创建DialogFragment

---
+++ app/src/main/java/com/bcy/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,17 @@
+package com.bcy.android.criminalintent;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class DatePickerFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
