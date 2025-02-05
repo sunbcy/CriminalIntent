a29f6b6c392c6916b1cb544feff7f69964b7d6e4
bcy <626813534@qq.com>
2025-02-06 01:33:31 +0800

    12-5 添加newInstance(Date)方法

--- app/src/main/java/com/bcy/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bcy/android/criminalintent/DatePickerFragment.java
@@ -5,10 +5,26 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
 
 import androidx.fragment.app.DialogFragment;
 
+import java.util.Date;
+
 public class DatePickerFragment extends DialogFragment {
+
+    private static final String ARG_DATE = "date";
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         View v = LayoutInflater.from(getActivity())
