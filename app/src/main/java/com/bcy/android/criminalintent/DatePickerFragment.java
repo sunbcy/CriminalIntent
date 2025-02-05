1eb1cc1b4bf04c44d0a70575d6a572cf666ffe5d
bcy <626813534@qq.com>
2025-02-06 00:48:14 +0800

    12-4 给AlertDialog添加DatePicker

--- app/src/main/java/com/bcy/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bcy/android/criminalintent/DatePickerFragment.java
@@ -3,13 +3,19 @@ package com.bcy.android.criminalintent;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.fragment.app.DialogFragment;
 
 public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
