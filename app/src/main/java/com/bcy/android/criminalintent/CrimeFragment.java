f3035a096f7849e2881db8cb9345eae79090621c
bcy <626813534@qq.com>
2025-02-07 22:26:39 +0800

    12-8 设置目标fragment

--- app/src/main/java/com/bcy/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bcy/android/criminalintent/CrimeFragment.java
@@ -19,6 +19,9 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
+
+    private static final int REQUEST_DATE = 0;
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
@@ -56,6 +59,7 @@ public class CrimeFragment extends Fragment {
                 FragmentManager manager = getFragmentManager();
 //                DatePickerFragment dialog = new DatePickerFragment();
                 DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);  // 设置目标fragment 建立fragment之间的联系
                 dialog.show(manager, DIALOG_DATE);
             }
         });
