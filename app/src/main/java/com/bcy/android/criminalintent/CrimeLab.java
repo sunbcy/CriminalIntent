3d5eec1d466de8ff97b3b1f977517c9d3878fd2a
bcy <626813534@qq.com>
2025-02-16 00:41:29 +0800

    14-4 打开SQLiteDatabase

--- app/src/main/java/com/bcy/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bcy/android/criminalintent/CrimeLab.java
@@ -1,6 +1,7 @@
 package com.bcy.android.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,8 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;  // 创建单例。sCrimeLab变量的s前缀代表静态变量,这是Android开发的命名约定
 
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {  // CrimeLab的私有构造方法
         if (sCrimeLab == null) {
@@ -19,6 +22,8 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
         mCrimes = new ArrayList<>();
 
 //        for (int i = 0; i < 100; i++) {
