0088801c94ff337496cf3b213791422ac16c1450
bcy <626813534@qq.com>
2025-02-16 01:49:27 +0800

    14-6 创建crime数据表

--- app/src/main/java/com/bcy/android/criminalintent/CrimeBaseHelper.java
+++ app/src/main/java/com/bcy/android/criminalintent/CrimeBaseHelper.java
@@ -16,7 +16,7 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + CrimeTable.NAME);
+        db.execSQL("create table " + CrimeTable.NAME + "(" + " _id integer primary key autoincrement, " + CrimeTable.Cols.UUID + ", " + CrimeTable.Cols.TITLE + ", " + CrimeTable.Cols.DATE + ", " + CrimeTable.Cols.SOLVED + ")");
     }
 
     @Override
