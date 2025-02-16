0caea8e00adabc4dc36efbbd33f995f5779f2555
bcy <626813534@qq.com>
2025-02-16 17:55:24 +0800

    14-20 添加setCrimes(List<Crime>)方法

--- app/src/main/java/com/bcy/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bcy/android/criminalintent/CrimeListFragment.java
@@ -89,6 +89,10 @@ public class CrimeListFragment extends Fragment {
         public int getItemCount() {
             return mCrimes.size();
         }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
     }
 
     @Override
