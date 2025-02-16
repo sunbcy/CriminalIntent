8cc84e5753c3802dfa40b08cbbc419433de896f7
bcy <626813534@qq.com>
2025-02-17 02:12:46 +0800

    15-9 发送消息

--- app/src/main/java/com/bcy/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bcy/android/criminalintent/CrimeFragment.java
@@ -30,6 +30,7 @@ public class CrimeFragment extends Fragment {
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
+    private Button mReportButton;
 
     public static  CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -101,6 +102,18 @@ public class CrimeFragment extends Fragment {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 mCrime.setSolved(isChecked);
+            }
+        });
+
+        mReportButton = (Button) v.findViewById(R.id.crime_report);
+        mReportButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
+                i.putExtra(Intent.EXTRA_SUBJECT,
+                        getString(R.string.crime_report_subject));
+                startActivity(i);
             }
         });
 
