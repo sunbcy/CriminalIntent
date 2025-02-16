3f4acf19da706c40f5c1480488870b42b48cb541
bcy <626813534@qq.com>
2025-02-16 16:06:33 +0800

    14-14 新增getCrime()方法

--- app/src/main/java/com/bcy/android/criminalintent/CrimeCursorWrapper.java
+++ app/src/main/java/com/bcy/android/criminalintent/CrimeCursorWrapper.java
@@ -7,4 +7,13 @@ public class CrimeCursorWrapper extends CursorWrapper {
     public CrimeCursorWrapper(Cursor cursor) {
         super(cursor);
     }
+
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.SOLVED));
+
+        return null;
+    }
 }
