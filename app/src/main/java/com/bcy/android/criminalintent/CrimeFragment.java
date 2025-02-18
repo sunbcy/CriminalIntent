17d3bb5a41de2c4cae5aa14de958951599463669
bcy <626813534@qq.com>
2025-02-19 00:38:32 +0800

    15-15 过滤器验证代码

--- app/src/main/java/com/bcy/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bcy/android/criminalintent/CrimeFragment.java
@@ -125,6 +125,7 @@ public class CrimeFragment extends Fragment {
         });
 
         final Intent pickContact = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        pickContact.addCategory(Intent.CATEGORY_HOME);
         mSuspectButton = (Button) v.findViewById(R.id.crime_suspect);
         mSuspectButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
