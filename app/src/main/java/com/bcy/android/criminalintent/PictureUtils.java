f8b178c613438a533fdd311f6138d6e36f8a881a
bcy <626813534@qq.com>
2025-02-23 11:04:45 +0800

    16-10 编写合理的缩放方法;

--- app/src/main/java/com/bcy/android/criminalintent/PictureUtils.java
+++ app/src/main/java/com/bcy/android/criminalintent/PictureUtils.java
@@ -1,9 +1,17 @@
 package com.bcy.android.criminalintent;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
 
 public class PictureUtils {
+    public static Bitmap getScaledBitmap(String path, Activity activity) {
+        Point size = new Point();
+        activity.getWindowManager().getDefaultDisplay().getSize(size);
+        return getScaledBitmap(path, size.x, size.y);
+    }
+
     public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight) {
         // Read in the dimensions of the image on disk
         BitmapFactory.Options options = new BitmapFactory.Options();
